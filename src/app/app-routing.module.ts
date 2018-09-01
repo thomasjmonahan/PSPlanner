c310b8f38553b0c1eac00a92ef1de5fb0e534107
thomasjmonahan <tmonahan2424@gmail.com>
2018-09-01 12:32:20 -0400

    home backup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import {AssessmentComponent} from './assessment/assessment.component';
+import {HomeComponent} from './home/home.component';
+import {LearnComponent} from './learn/learn.component';
+import {TeachComponent} from './teach/teach.component';
+import {ShareComponent} from './share/share.component';
+const routes: Routes = [
+  {
+    path:'assessment',
+    component: AssessmentComponent
+  },
+  {
+    path:'',
+    component: HomeComponent
+  },
+  {
+    path:'share',
+    component: ShareComponent
+  },
+  {
+    path:'teach',
+    component: TeachComponent
+  },
+  {
+    path:'learn',
+    component: LearnComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { AssessmentComponent } from './assessment/assessment.component';
+import { HomeComponent } from './home/home.component';
+import { TeachComponent } from './teach/teach.component';
+import { LearnComponent } from './learn/learn.component';
+import { ShareComponent } from './share/share.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    AssessmentComponent,
+    HomeComponent,
+    TeachComponent,
+    LearnComponent,
+    ShareComponent
   ],
   imports: [
     BrowserModule,
